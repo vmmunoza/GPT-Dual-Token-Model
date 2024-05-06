1038dbeefdc7b5a0ed9cba25f74f71265921b5bb
Víctor Muñoz <53861440+vmmunoza@users.noreply.github.com>
2024-05-06 11:51:25 +0200

    Update TokenContract.sol

--- TokenContract.sol
+++ TokenContract.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract YointsToken is ERC20, Ownable {
+    mapping(address => bool) private _blacklist;
+
+    constructor() ERC20("Yoints Token", "YOINTS") {
+        _mint(msg.sender, 100_000_000 * 10**18); // Minting 100 million YOINTS tokens
+    }
+
+    function blacklistAddress(address account) public onlyOwner {
+        _blacklist[account] = true;
+    }
+
+    function unblacklistAddress(address account) public onlyOwner {
+        _blacklist[account] = false;
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(!_blacklist[msg.sender], "Sender is blacklisted");
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(!_blacklist[sender], "Sender is blacklisted");
+        return super.transferFrom(sender, recipient, amount);
+    }
+}
